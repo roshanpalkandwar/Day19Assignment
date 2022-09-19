317f49735081a6fcc8f27e6088855b7418c5d0b8
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-19 13:26:35 +0530

    [roshan]valid first name

---
+++ Day19Assignment/FirstNameRegex.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19Assignment
+{
+    internal class FirstNameRegex
+    {
+        public static void ValidateFirstName()
+        {
+            Console.WriteLine("Enter first name");
+            string firstName = Console.ReadLine();
+
+            string pattern = "^[A-Z]{1}[a-z]{2,}$";
+            if (Regex.IsMatch(firstName, pattern))
+            {
+                Console.WriteLine("Valid first name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid first name... Please enter a valid first name");
+            }
+        }
+    }
+}
---
+++ Day19Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Day19Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number\n1.First Name Regex");
+            int obj=Convert.ToInt32(Console.ReadLine());    
+
+            switch(obj)
+            {
+                case 1:
+                    FirstNameRegex.ValidateFirstName();
+                    break;
+            }
+           
+        }
+    }
+}
